2d3f3952585440d208177b5a20059b5eae8ece1a
George <dyedpurple@googlemail.com>
2017-04-01 21:41:14 +0100

    Initial Commit

---
+++ tetris.py
@@ -0,0 +1,35 @@
+import sys
+import random
+
+import pygame
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+
+pygame.init()
+NUM_ROWS = 40
+NUM_COLS = 10
+
+SQUARE_WIDTH = 10
+SQUARE_HEIGHT = 10
+MARGIN = 1
+SIZE = [(NUM_COLS * SQUARE_WIDTH) + ((NUM_COLS + 1) * MARGIN), (NUM_ROWS * SQUARE_HEIGHT) + ((NUM_ROWS + 1) * MARGIN)]
+SCREEN = pygame.display.set_mode(SIZE)
+pygame.display.set_caption("Tetris")
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    # Draw grid:
+    SCREEN.fill(BLACK)
+    for x in xrange(0, NUM_ROWS):
+        for y in xrange(0, NUM_ROWS):
+            pygame.draw.rect(SCREEN, WHITE, [((MARGIN + SQUARE_WIDTH) * x) + MARGIN, ((MARGIN + SQUARE_HEIGHT) * y) + MARGIN, SQUARE_WIDTH, SQUARE_HEIGHT])
+
+    pygame.display.update()
+    clock.tick(10)
