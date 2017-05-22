806c21b483eb28f7e3c5f6d7e7e19dbc76c14720
George <dyedpurple@googlemail.com>
2017-05-23 00:33:01 +0100

    Make class to represent board

---
+++ board_class.py
@@ -0,0 +1,19 @@
+class Board():
+
+    def __init__(self):
+        self.num_rows = 40
+        self.num_cols = 10
+
+        self.square_width = 10
+        self.square_height = 10
+        self.margin = 1
+
+        combined_width_cells = self.num_cols * self.square_width
+        combined_width_margin = (self.num_cols + 1) * self.margin
+        total_self_width = combined_width_cells + combined_width_margin
+        
+        combined_height_cells = self.num_rows * self.square_height
+        combined_height_margin = (self.num_rows + 1) * self.margin
+        total_self_height = combined_height_cells + combined_height_margin
+
+        self.size = [total_self_width, total_self_height]
