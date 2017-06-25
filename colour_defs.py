05d3d7153b17349f240d9fee95f5c25b6671be0d
George <dyedpurple@googlemail.com>
2017-06-25 11:51:49 +0100

    Add fcn to speciy piece's colour

--- colour_defs.py
+++ colour_defs.py
@@ -1,3 +1,11 @@
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
+CYAN = (0, 255, 255)
+YELLOW = (255, 255, 0)
+
+def calc_colour(piece_type):
+    if piece_type == "STRAIGHT":
+        return CYAN
+    elif piece_type == "SQUARE":
+        return YELLOW
